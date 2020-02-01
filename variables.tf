53abbcf9c87c37a8370a429d677e84bb941a69fc
devoptimist <sbrown@chef.io>
2020-02-01 20:44:10 +0000

    update to use chef_automate_wrapper v0.1.9-20200201203939

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "timeout" {
 variable "cookbooks" {
   description = "the cookbooks used to deploy chef automate"
   default = {
-    "chef_automate_wrapper" = "github: 'srb3/chef_automate_wrapper', tag: 'v0.1.9'",
+    "chef_automate_wrapper" = "github: 'srb3/chef_automate_wrapper', tag: 'v0.1.9-20200201203939'",
     "chef-ingredient" = "github: 'srb3/chef-ingredient', branch: 'products'"
   }
 }
